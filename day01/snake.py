42a57fb3be707ee92a35ba1d98309b04958bba4b
Virginia Walthers <walthvir000@warren.k12.in.us>
2025-11-10 08:20:59 -0500

    starter variables made

--- day01/snake.py
+++ day01/snake.py
@@ -15,7 +15,7 @@ clock = pygame.time.Clock()
 # Set game values
 SNAKE_SIZE = 20
 head_x = WINDOW_WIDTH // 2
-head_y = WINDOW_HEIGHT + 100 // 2
+head_y = WINDOW_HEIGHT // 2 + 100
 snake_dx = 0
 snake_dy = 0
 score = 0
